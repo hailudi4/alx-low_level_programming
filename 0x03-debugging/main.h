af236d859bfa8b3275f8c8c8fec3bcee39022d50
hailudi4 <hailudi4@gmail.com>
2022-12-14 22:41:57 -0800

    Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining-days(int month, int day, int year);
+int convert-day(int month, int day);
+void print_times_table(int n);
+
+#endif
